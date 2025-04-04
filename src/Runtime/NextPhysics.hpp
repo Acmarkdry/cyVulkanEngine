b822aa6e8bad3ea189e9c2ce0026aadadb9ce5be
acmarkdry <acmarkdry@163.com>
2025-04-04 11:27:48 +0800

    # physics代码提交

---
+++ src/Runtime/NextPhysics.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+#include <Jolt/Jolt.h>
+#include <Jolt/Math/Math.h>
+#include <Jolt/Physics/Body/BodyID.h>
+#include <Jolt/Physics/Body/MotionType.h>
+
+#include "Vulkan/vulkan.hpp"
+
+struct FNextPhysicsContext;
+
+enum class ENextBodyShape
+{
+	Box,
+	Sphere,
+	Capsule,
+	Cylinder,
+	Cone,
+	ConvexHull,
+	ConvexDecomposition,
+	HeightField,
+	Mesh,
+	SoftBody
+};
+
+struct FNextPhysicsBody
+{
+	glm::vec3 position;
+	glm::vec3 velocity;
+	ENextBodyShape shape;
+	JPH::BodyID bodyID;
+};
+
+
+class NextPhysics final
+{
+public:
+	VULKAN_NON_COPIABLE(NextPhysics)
+
+	NextPhysics();
+	~NextPhysics();
+
+	void Start();
+	void Tick(double DeltaSeconds);
+	void Stop();
+
+	JPH::BodyID CreateSphereBody(glm::vec3 position, float radius, JPH::EMotionType motionType);
+	JPH::BodyID CreatePlaneBody(glm::vec3 position, glm::vec3 extent, JPH::EMotionType motionType);
+
+	FNextPhysicsBody* GetBody(JPH::BodyID bodyID);
+
+	void OnSceneStarted();
+	void OnSceneEnded();
+
+private:
+	JPH::BodyID AddBodyInternal(FNextPhysicsBody& body);
+
+	std::unique_ptr<FNextPhysicsContext> context_;
+	std::unordered_map<JPH::BodyID, FNextPhysicsBody> bodies_;
+
+	double TimeElapsed {};
+	double TimeSimulated {};
+};
